f09ba4560a78250f07346a3b7eb18b91484a458c
ArthurHlt <arthurh.halet@gmail.com>
2016-05-05 21:07:05 +0200

    add pup to base image

--- mattermost/tasks/check-version.sh
+++ mattermost/tasks/check-version.sh
@@ -2,9 +2,6 @@
 
 CW=$PWD
 
-curl https://github.com/ericchiang/pup/releases/download/v0.3.9/pup_linux_amd64.zip -o pup_linux_amd64.zip
-unzip pup_linux_amd64.zip
-chmod +x pup
 actual_version=$(curl -s http://www.mattermost.org/download/ | ./pup '.entry-content h2' | grep Mattermost | awk '{print $4}')
 current_version=$(head -n 1 "$CW/mattermost-integrator-github/tag")
 if [ "$actual_version" = "$current_version" ]; then
