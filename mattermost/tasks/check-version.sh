3b6edc7a2f724a0d395a55f23c5714c94fa7a44d
ArthurHlt <arthurh.halet@gmail.com>
2016-05-05 21:41:44 +0200

    add check for parsing in mattermost

--- mattermost/tasks/check-version.sh
+++ mattermost/tasks/check-version.sh
@@ -9,6 +9,11 @@ if [ "$actual_version" = "$current_version" ]; then
     exit 1
 fi
 semver=$(echo "$actual_version" | sed 's/v//g')
+echo "$semver" | grep -Eq "^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}$"
+if [ $? -ne 0 ]; then
+    echo "Parsed version do not follow semantical versionning, check http://www.mattermost.org/download page and fix the parsing"
+    exit 1
+fi
 echo "$semver" > "$CW/mattermost-integrator-github/tag_to_release"
 echo "Mattermost $semver on Cloud Foundry" > "$CW/mattermost-integrator-github/name_of_release"
 exit 0
